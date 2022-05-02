1fa8d9d76de9f75ae631ac193eee7bc5f919311a
Imran Khan <imrankhanswati@gmail.com>
2022-05-02 19:47:13 +0200

    installed vuex

---
+++ app-frontend/src/store/index.js
@@ -0,0 +1,18 @@
+import { createStore } from "vuex";
+
+const store = createStore({
+  state() {
+    return {
+      count: 0,
+    };
+  },
+  getters: {},
+  actions: {
+    
+  },
+  mutations: {
+  },
+
+});
+
+export default store
